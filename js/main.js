1bafd32e71faf56dcf170eaca9cffd603b28a64a
Sean Soper <sean.soper@gmail.com>
2019-12-29 17:56:48 -0500

    updated site arch to be a bit more sane

---
+++ js/main.js
@@ -0,0 +1,46 @@
+var chooseTheme = function() {
+  var places = [
+    'carlsbad',
+    'singapore',
+    'potomac',
+    'taipei',
+    'kaohsiung'
+  ];
+
+  var place = places[Math.floor(Math.random()*places.length)];
+  document.body.classList.add(place);
+};
+
+var loadDeferredStyles = function() {
+  chooseTheme()
+  var addStylesNode = document.getElementById('deferred-styles');
+  var replacement = document.createElement('div');
+  replacement.innerHTML = addStylesNode.textContent;
+  document.body.appendChild(replacement)
+  addStylesNode.parentElement.removeChild(addStylesNode);
+};
+
+var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
+else window.addEventListener('load', loadDeferredStyles);
+
+// Cheers! 🍻
+
+var str = '';
+
+$(document).keydown(function(evt) {
+    // debug
+    // console.log(evt.keyCode)
+    if (evt.keyCode === 27) {
+        str = '';
+    }
+});
+
+$(document).keypress(function(evt) {
+    str += evt.keyCode;
+
+    if (str === '98101101114') {
+        $('#untappd').removeAttr('class');
+    }
+});
